f6a3571436b0f5392eae9325abf6f648fef445ec
Kenichi Beveridge <kenbev84@gmail.com>
2022-09-12 21:19:41 +0100

    Add scaffold and authors controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :authors
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,4 +7,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "home#index"
+  scope module: 'authors' do
+    resources :posts
+  end
 end
